e911a6bd0a96f38990a4558e9bb290cde03ebfd3
K0fii <kqfi26@gmail.com>
2025-12-22 12:25:21 +0100

    Aplikacja desktopowa

--- desktopowe/PrzegladarkaObiektow/MainWindow.xaml.cs
+++ desktopowe/PrzegladarkaObiektow/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace PrzegladarkaObiektow;
 
@@ -16,8 +18,66 @@ namespace PrzegladarkaObiektow;
 /// </summary>
 public partial class MainWindow : Window
 {
+    List<Pracownik> pracownicy = new List<Pracownik>();
+    int index = 0;
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void wczytajDane(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        if(openFileDialog.ShowDialog() == true)
+        {
+            pracownicy.Clear();
+            string[] linie = File.ReadAllLines(openFileDialog.FileName);
+
+            foreach (string line in linie)
+            {
+                string[] dane = line.Split(';');
+                pracownicy.Add(new Pracownik(
+                    Convert.ToInt32(dane[0]),
+                    dane[1],
+                    dane[2],
+                    dane[3],
+                    Convert.ToDouble(dane[4])
+                    ));
+            }
+
+            wyswietlPracownika(0);
+        }
+    }
+
+    private void wyswietlPracownika(int id)
+    {
+        Pracownik p = pracownicy[id];
+        lbImie.Content = $"Pracownik: {p.Imie} {p.Nazwisko}";
+        lbZawod.Content = $"Zawód: {p.Zawod}";
+        lbPlaca.Content = $"Placa: {p.Placa}";
+        Uri uri = new Uri($"img/{p.Zdjecie}", UriKind.Relative);
+        imgPracownik.Source = new BitmapImage(uri);
+    }
+    private void poprzedniPracownik(object sender, RoutedEventArgs e)
+    {
+        if (pracownicy.Count > 0)
+        {
+            if (index > 0)
+                index--;
+            wyswietlPracownika(index);
+        }
+        else return;
+    }
+
+    private void nastepnyPracownik(object sender, RoutedEventArgs e)
+    {
+        if(pracownicy.Count > 0)
+        {
+            if (index < 2)
+                index++;
+            wyswietlPracownika(index);
+        }
+        else return;
     }
 }
---
+++ desktopowe/PrzegladarkaObiektow/Pracownik.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzegladarkaObiektow
+{
+    public class Pracownik
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Zawod { get; set; }
+        public double Placa { get; set; }
+        public string Zdjecie { get; set; }
+
+        public Pracownik(int  id, string imie, string nazwisko, string zawod, double placa)
+        {
+            Id = id;
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Zawod = zawod;
+            Placa = placa;
+            Zdjecie = $"{id}-foto.jpg";
+        }
+    }
+}
