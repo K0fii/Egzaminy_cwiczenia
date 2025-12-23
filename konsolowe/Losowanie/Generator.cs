101db91fa0e482c401b60da6e3eadd5de98ba413
K0fii <kqfi26@gmail.com>
2025-12-23 10:54:20 +0100

    Aplikacja konsolowa

---
+++ konsolowe/Losowanie/Generator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Losowanie
+{
+    public class Generator
+    {
+        public static int[,] generujTablice()
+        {
+
+            HashSet<int> wylosowaneLiczby = new HashSet<int>();
+            Random random = new Random();
+
+            while(wylosowaneLiczby.Count < 25)
+            {
+                wylosowaneLiczby.Add(random.Next(1, 51));
+            }
+        
+            int[] tmp = new int[25];
+            int index = 0;
+            foreach (int liczba in wylosowaneLiczby)
+            {
+                tmp[index] = liczba;
+                index++;
+            }
+            int[,] tablica = new int[5, 5];
+            for(int i=0; i<5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    tablica[i, j] = tmp[(5*i)+j];
+                }
+            }
+            return tablica;
+        }
+    }
+}
---
+++ konsolowe/Losowanie/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Losowanie
+{
+    internal class Program
+    {
+        public static void wyswietl(int[,] tab)
+        {
+            Console.WriteLine("---------------------");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("{0 , 4}",tab[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("----------------------");
+        }
+        static void Main(string[] args)
+        {
+            int[,] tab = Generator.generujTablice();
+
+            wyswietl(tab);
+
+
+            Console.ReadKey();
+        }
+    }
+}
